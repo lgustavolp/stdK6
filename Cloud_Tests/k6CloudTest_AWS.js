c6d30afc89a9d19535a50fffbcb79bc2748bb7f4
Luiz Gustavo Lucena <lgustavolp@gmail.com>
2025-08-03 17:41:09 -0300

    Update yml and test file

--- Cloud_Tests/k6CloudTest_AWS.js
+++ Cloud_Tests/k6CloudTest_AWS.js
@@ -1,6 +1,7 @@
 
 import http from 'k6/http';
 import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     vus: 100,
@@ -18,4 +19,9 @@ export default function(){
     check(res, {
         'status code 200': (r) => r.status === 200
     });
+}
+export function handleSummary(data) {
+  return {
+    "index.html": htmlReport(data),
+  };
 }
